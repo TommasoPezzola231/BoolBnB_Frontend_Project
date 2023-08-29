be14ed588bb88285e927a6335018e910ab56c2c9
LorenzoSeverini <lorenzo.seve98@gmail.com>
2023-08-29 14:36:41 +0200

    feat data

--- src/store.js
+++ src/store.js
@@ -3,13 +3,13 @@ import { reactive } from 'vue'
 import axios from 'axios';
 
 export const store = reactive({
+    // path - api
     serverPath: 'http://localhost:8000',
+    
     // map - geo location
     lat: '',
     lon: '',
     address: '',
     range: 20,
     apiKey: process.env.VUE_APP_TOMTOM_API_KEY,
-
-
 });
