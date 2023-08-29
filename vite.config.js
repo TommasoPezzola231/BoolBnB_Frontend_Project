0a960e9be7ffecb01549043319bba93e375cc461
LorenzoSeverini <lorenzo.seve98@gmail.com>
2023-08-29 14:21:42 +0200

    feaat additional data scss

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: ` 
+        @import "./src/styles/partials/_variables.scss";
+        @import "./src/styles/partials/_mixins.scss";
+        `
+      },
+    },
+  }
 })
