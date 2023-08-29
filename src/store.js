db94cffc820b5511bb972861266e75e7d0235af4
LorenzoSeverini <lorenzo.seve98@gmail.com>
2023-08-29 15:57:41 +0200

    feat server apartmetn path

--- src/store.js
+++ src/store.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 export const store = reactive({
     // path - api
     serverPath: 'http://localhost:8000',
-    
+    serverApartmentsPath: 'http://localhost:8000/api/apartments',
     // map - geo location
     lat: '',
     lon: '',
