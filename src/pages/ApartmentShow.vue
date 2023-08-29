369ea6f2cc6a43009d62b7aceed9f5e7734f6cbc
LorenzoSeverini <lorenzo.seve98@gmail.com>
2023-08-29 14:22:51 +0200

    feat apartment show

---
+++ src/pages/ApartmentShow.vue
@@ -0,0 +1,37 @@
+<script>
+
+import axios from 'axios';
+import HeaderApp from '../components/header/HeaderApp.vue'
+import FooterApp from '../components/footer/FooterApp.vue'
+
+export default {
+    name: 'ApartmentShow',
+    components: {
+        HeaderApp,
+        FooterApp,
+    },
+
+    data () {
+        return {
+            // 
+        };
+    },
+}
+</script>
+
+<template>
+   <section>
+        <h1>Apartments Show</h1>
+        <h2>visualizza il sigolo appartamento con tutti i dati, immagini, mappa della posizione dell'appartamento</h2>
+        <h2>form per contattare lo user proprietario dell'appartamento</h2>
+   </section>
+</template>
+
+<style lang="scss" scoped>
+@import '../styles/main.scss';
+
+section {
+    padding: 2rem;
+    // giusto per staccare dai bordi
+}
+</style>
