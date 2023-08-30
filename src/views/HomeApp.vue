2e177bc5a074af3fc2ba90e244aad967ba2ebeb6
LorenzoSeverini <lorenzo.seve98@gmail.com>
2023-08-30 13:08:26 +0200

    fix

--- src/views/HomeApp.vue
+++ src/views/HomeApp.vue
@@ -36,23 +36,23 @@ export default {
             <!-- benvenuto -->
             <div class="row">
                 <div class="col-12">
-                    <h1 class="display-1">Benvenuto su BoolBnB</h1>
-                    <p class="lead">Trova un appartamento per le tue vacanze o per un soggiorno di lavoro</p>
+                    <h1>Benvenuto su BoolBnB</h1>
+                    <p class="lead text-white">Trova un appartamento per le tue vacanze o per un soggiorno di lavoro</p>
                 </div>
             </div>
             <!-- cerca citta -->
             <div class="row">
-                <div class="p-5 mb-4 bg-dark text-light rounded-3">
+                <div class="p-5 mb-4 text-light rounded-3 my_bg-jumbo">
                     <div class="container-fluid py-5">
                         <h1 class="display-5 fw-bold">Cerca il tuo prossimo soggiorno</h1>
                         <p class="col-md-8 fs-4 mt-3">Inserisci il nome della città</p>
                         <form>
-                            <div class="form-floating mb-3">
+                            <div class="form-floating mb-3 col-md-8 col-lg-5">
                                 <input type="text" class="form-control text-dark" id="floatingInput" placeholder="Roma">
                                 <label for="floatingInput">Città</label>
                             </div>
                             <!-- bottone cerca -->
-                            <router-link :to="{ name: 'apartments-index' }" class="btn btn-primary btn-lg">
+                            <router-link :to="{ name: 'apartments-index' }" class="btn btn-lg my_btn">
                                 Cerca
                             </router-link>
                         </form>
@@ -71,5 +71,22 @@ export default {
 
 <style lang="scss" scoped>
 @import '../styles/main.scss';
+
+.my_bg-jumbo {
+    background-color: $bg-color-dark;
+
+    .my_btn {
+        background-color: $color-primary;
+        border-color: $color-primary;
+        border: none;
+        transition: all 0.3s ease-in-out;
+
+        &:hover {
+            background-color: $color-primary;
+            border-color: $color-primary;
+            border: none;
+        }
+    }
+}
 
 </style>
