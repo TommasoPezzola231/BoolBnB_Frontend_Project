4aa8a4c64cf50cdb1260ac133cea6e3e25b23087
LorenzoSeverini <lorenzo.seve98@gmail.com>
2023-08-30 21:08:45 +0200

    fix height problem

--- src/components/main/homepage/SponsoredApartments.vue
+++ src/components/main/homepage/SponsoredApartments.vue
@@ -23,7 +23,7 @@ export default {
             <div class="row row-cols-1 row-cols-md-2 g-4 mt-2">
                 <template v-for="apartment in apartments" :key="apartment.id">
                     <div class="col col-md-6 col-lg-4 col-xl-3">
-                        <div class="card text-white bg-dark h">
+                        <div class="card text-white bg-dark h-100 d-flex align-items-stretch">
                             <img :src="getImageUrl(apartment.principal_image)" class="card-img-top" alt="Apartment Image">
                             <div class="card-body">
                             <p class="card-title"><strong>{{ apartment.title }}</strong></p>
