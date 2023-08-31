b09f6ab3856aea8b4d0147a739a56ffc64893d5e
Valerio Billi <valerio.billi18@gmail.com>
2023-08-31 15:27:34 +0200

    fix notFound

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,17 +1,17 @@
 <script>
 export default {
-  name: 'NotFound',
+    name: 'NotFound',
 }
 </script>
 
 <template>
     <div class="container">
         <div class="row">
-            <div class="col-12">
-                <h1>404</h1>
+            <div class="col-12 d-flex flex-column justify-content-end align-items-center">
+                <!--qui img logo grande -->
+                <h1 class="mt-5">Error 404</h1>
                 <h2>Page not found</h2>
                 <p>Errore! La pagina che stai cercando non è stata trovata!</p>
-                <!-- img logo boolbnb  -->
                 <img src="logo" alt="logo" class="img-fluid" width="100px">
                 <router-link :to="{ name: 'home' }" class="align-items-center h100 d-flex text-decoration-none">
                     <div>
@@ -21,9 +21,6 @@ export default {
             </div>
         </div>
     </div>
-
 </template>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
