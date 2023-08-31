4663386baabb2d97962a22070fbd6452385f4cde
Valerio Billi <valerio.billi18@gmail.com>
2023-08-31 17:23:48 +0200

    add scaffold apartment show

--- src/views/ApartmentShow.vue
+++ src/views/ApartmentShow.vue
@@ -11,7 +11,7 @@ export default {
         FooterApp,
     },
 
-    data () {
+    data() {
         return {
             // 
         };
@@ -20,18 +20,76 @@ export default {
 </script>
 
 <template>
-   <section>
-        <h1>Apartments Show</h1>
-        <h2>visualizza il sigolo appartamento con tutti i dati, immagini, mappa della posizione dell'appartamento</h2>
-        <h2>form per contattare lo user proprietario dell'appartamento</h2>
-   </section>
+    <section>
+        <div class="container-fluid container-img">
+            <div class="row">
+                <div class="col">
+                    <h1>Titolo appartamento</h1>
+                    <div class="d-flex">Posizione</div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-6 mt-2">
+                    <div class="big-img-placeholder">Placeholder principal image</div>
+                </div>
+                <div class="col-6 mt-2">
+                    <div class="row">
+                        <div class="col-6 d-flex">
+                            <div class="small-img-placeholder">Placeholder principal image</div>
+                        </div>
+                        <div class="col-6">
+                            <div class="small-img-placeholder">Placeholder principal image</div>
+                        </div>
+                        <div class="col-6">
+                            <div class="small-img-placeholder">Placeholder principal image</div>
+                        </div>
+                        <div class="col-6">
+                            <div class="small-img-placeholder">Placeholder principal image</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-6">
+                    <p class="mt-4">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae corporis est,
+                        expedita vel
+                        commodi eveniet, maxime doloribus temporibus saepe laboriosam enim rem, molestias ea. Fuga, earum?
+                        Minus culpa ipsa ducimus? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate
+                        impedit illo, aut laudantium, deleniti porro, assumenda quasi cumque explicabo repudiandae at
+                        voluptatibus hic quo fuga tempore. Numquam non ad aut.</p>
+                </div>
+            </div>
+        </div>
+
+    </section>
 </template>
 
 <style lang="scss" scoped>
 @import '../styles/main.scss';
+@import '../styles/partials/variables';
 
 section {
     padding: 2rem;
     // giusto per staccare dai bordi
+}
+
+// placheholder debugging
+
+.container-img {
+    height: 400px;
+}
+
+.big-img-placeholder {
+    width: 100%;
+    background-color: bisque;
+    height: 400px;
+    border-radius: 20px;
+}
+
+.small-img-placeholder {
+    width: 100%;
+    background-color: bisque;
+    height: 200px;
+    border-radius: 20px;
 }
 </style>
