2af382e5fd9c202cddb81d56fceedbb2f50d8df6
TommasoPezzola <pezzola123@gmail.com>
2023-09-01 10:59:09 +0200

    aggiunte icone

--- src/main.js
+++ src/main.js
@@ -16,8 +16,46 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons'; 
 import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
 
+import { faWifi } from "@fortawesome/free-solid-svg-icons";
+import { faPersonSwimming } from "@fortawesome/free-solid-svg-icons";
+import { faDumbbell } from "@fortawesome/free-solid-svg-icons";
+import { faHotTubPerson } from "@fortawesome/free-solid-svg-icons";
+import { faSquareParking } from "@fortawesome/free-solid-svg-icons";
+import { faFan } from "@fortawesome/free-solid-svg-icons";
+import { faTemperatureArrowUp } from "@fortawesome/free-solid-svg-icons";
+import { faMugSaucer } from "@fortawesome/free-solid-svg-icons";
+import { faTv } from "@fortawesome/free-solid-svg-icons";
+import { faKitchenSet } from "@fortawesome/free-solid-svg-icons";
+import { faSoap } from "@fortawesome/free-solid-svg-icons";
+import { faShirt } from "@fortawesome/free-solid-svg-icons";
+import { faWind } from "@fortawesome/free-solid-svg-icons";
+import { faSmoking } from "@fortawesome/free-solid-svg-icons";
+import { faKitMedical } from "@fortawesome/free-solid-svg-icons";
+import { faDog } from "@fortawesome/free-solid-svg-icons";
+import { faFire } from "@fortawesome/free-solid-svg-icons";
+import { faWater } from "@fortawesome/free-solid-svg-icons";
+
+
 library.add(faGithub);
 library.add(faChevronDown);
+library.add(faWifi);
+library.add(faPersonSwimming);
+library.add(faDumbbell);
+library.add(faHotTubPerson);
+library.add(faSquareParking);
+library.add(faFan);
+library.add(faTemperatureArrowUp);
+library.add(faMugSaucer);
+library.add(faTv);
+library.add(faKitchenSet);
+library.add(faSoap);
+library.add(faShirt);
+library.add(faWind);
+library.add(faSmoking);
+library.add(faKitMedical);
+library.add(faDog);
+library.add(faFire);
+library.add(faWater);
 
 
 
--- src/views/ApartmentShow.vue
+++ src/views/ApartmentShow.vue
@@ -13,9 +13,24 @@ export default {
 
     data() {
         return {
-            // 
+            ApiUrl: "http://localhost:8000/api/apartments/",
+            apartment: []
         };
     },
+    methods: {
+        getProject() {
+            axios.get(this.ApiUrl + this.$route.params.id).then(result =>{
+                console.log(result.data.apartment)
+                this.apartment = result.data.apartment
+            }).catch(err => {
+                this.$router.push( { name: "not-found" } )
+            })
+        },
+    },
+    created() {
+        this.getProject()
+        console.log(this.apartment)
+    }
 }
 </script>
 
@@ -24,8 +39,8 @@ export default {
         <div class="container-fluid container-img">
             <div class="row">
                 <div class="col">
-                    <h1>Titolo appartamento</h1>
-                    <div class="d-flex">Posizione</div>
+                    <h1>{{ apartment.title }}</h1>
+                    <div class="d-flex">{{ apartment.address }}</div>
                 </div>
             </div>
             <div class="row">
@@ -51,12 +66,15 @@ export default {
             </div>
             <div class="row">
                 <div class="col-6">
-                    <p class="mt-4">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae corporis est,
-                        expedita vel
-                        commodi eveniet, maxime doloribus temporibus saepe laboriosam enim rem, molestias ea. Fuga, earum?
-                        Minus culpa ipsa ducimus? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate
-                        impedit illo, aut laudantium, deleniti porro, assumenda quasi cumque explicabo repudiandae at
-                        voluptatibus hic quo fuga tempore. Numquam non ad aut.</p>
+                    <p class="mt-4">{{ apartment.description }}</p>
+                </div>
+                <div class="col-6">
+                    <template v-for="service in apartment.services">
+                        <p class="col m-1 p-1 ">
+                            <span>{{ service.name_service }}</span>
+                            <font-awesome-icon :icon="service.icon_service" />
+                        </p>
+                    </template>
                 </div>
             </div>
         </div>
@@ -91,5 +109,11 @@ section {
     background-color: bisque;
     height: 200px;
     border-radius: 20px;
+}
+
+.fa-solid {
+    font-size: 15px;
+    color: white;
+    background-color: red;
 }
 </style>
