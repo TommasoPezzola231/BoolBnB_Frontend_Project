264a11610dba5168eb557ce1c56d0bde89aacba1
MarikaDiGirolamo <marikadigirolamo@gmail.com>
2023-09-04 15:29:39 +0200

    Add logo

--- src/components/header/HeaderApp.vue
+++ src/components/header/HeaderApp.vue
@@ -3,9 +3,9 @@
 export default {
   name: 'HeaderApp',
 
-  data () {
-    return { 
-      title : 'BoolBnB',
+  data() {
+    return {
+      title: 'BoolBnB',
     }
   },
 }
@@ -15,60 +15,64 @@ export default {
 <template>
   <!-- header using bootstrap  -->
   <div class="container-fluid h-100">
-      <div class="row h-100">
-        <div class="col d-flex justify-content-center justify-content-between align-items-center h-100">
-          <router-link :to="{ name: 'home' }" class="align-items-center h-100 d-flex text-decoration-none gap-2">
-            <div class="">
-              <img src="logo" alt="logo" class="img-fluid" width="100px">
-            </div>
-            <div>
-              <h1 class="my_title_color_text">{{ title }}</h1>
-            </div>
-          </router-link>
+    <div class="row h-100">
+      <div class="col d-flex justify-content-center justify-content-between align-items-center h-100">
+        <router-link :to="{ name: 'home' }" class="align-items-center h-100 d-flex text-decoration-none gap-2">
+          <div class="logo">
+            <!--percorso immagine-->
+            <img src="/src/assets/logo/Bool BnB_Black.png" class="logo img-fluid" alt="Logo">
 
-          <!-- xl - lg desktop -->
-          <div class="d-flex justify-content-end d-none d-lg-inline">
-            <div class="d-flex justify-content-center gap-3">
-              <router-link :to="{ name: 'home' }" class="align-items-center h100 d-flex text-decoration-none">
-                <div>
-                  <h4 class="my_header_color_text">Home</h4>
-                </div>
-              </router-link>
-    
-              <router-link :to="{ name: 'apartments-index' }" class="align-items-center h100 d-flex text-decoration-none">
-                <div>
-                  <h4 class="my_header_color_text">Vai alla ricerca</h4>
-                </div>
-              </router-link>
-            </div>
           </div>
-          <!-- tablet - mobile  -->
-          <div class="d-flex justify-content-end d-lg-none">
-            <div class="dropdown">
-              <button class="my_btn_dropdown d-flex align-content-center align-items-center gap-2 p-2" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-                <span><strong>Menu</strong></span>
-                <font-awesome-icon :icon="['fa-solid', 'chevron-down']" size="sm" />
-              </button>
-              <ul class="dropdown-menu mt-2 p-1 my_dropdown_style" aria-labelledby="dropdownMenuButton1">
-                <li>
-                  <router-link :to="{ name: 'home' }" class="align-items-center h100 d-flex text-decoration-none">
-                    <div>
-                      <h4 class="my_header_color_text">Home</h4>
-                    </div>
-                  </router-link>
-                </li>
-                <li>
-                  <router-link :to="{ name: 'apartments-index' }" class="align-items-center h100 d-flex text-decoration-none">
-                    <div>
-                      <h4 class="my_header_color_text">Vai alla ricerca</h4>
-                    </div>
-                  </router-link>
-                </li>
-              </ul>
-            </div>
+          <div>
+            <h1 class="my_title_color_text">{{ title }}</h1>
+          </div>
+        </router-link>
+
+        <!-- xl - lg desktop -->
+        <div class="d-flex justify-content-end d-none d-lg-inline">
+          <div class="d-flex justify-content-center gap-3">
+            <router-link :to="{ name: 'home' }" class="align-items-center h100 d-flex text-decoration-none">
+              <div>
+                <h4 class="my_header_color_text">Home</h4>
+              </div>
+            </router-link>
+
+            <router-link :to="{ name: 'apartments-index' }" class="align-items-center h100 d-flex text-decoration-none">
+              <div>
+                <h4 class="my_header_color_text">Vai alla ricerca</h4>
+              </div>
+            </router-link>
+          </div>
+        </div>
+        <!-- tablet - mobile  -->
+        <div class="d-flex justify-content-end d-lg-none">
+          <div class="dropdown">
+            <button class="my_btn_dropdown d-flex align-content-center align-items-center gap-2 p-2" type="button"
+              id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+              <span><strong>Menu</strong></span>
+              <font-awesome-icon :icon="['fa-solid', 'chevron-down']" size="sm" />
+            </button>
+            <ul class="dropdown-menu mt-2 p-1 my_dropdown_style" aria-labelledby="dropdownMenuButton1">
+              <li>
+                <router-link :to="{ name: 'home' }" class="align-items-center h100 d-flex text-decoration-none">
+                  <div>
+                    <h4 class="my_header_color_text">Home</h4>
+                  </div>
+                </router-link>
+              </li>
+              <li>
+                <router-link :to="{ name: 'apartments-index' }"
+                  class="align-items-center h100 d-flex text-decoration-none">
+                  <div>
+                    <h4 class="my_header_color_text">Vai alla ricerca</h4>
+                  </div>
+                </router-link>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
+    </div>
   </div>
 </template>
 
@@ -78,6 +82,7 @@ export default {
 .my_title_color_text {
   color: $color-text-primary;
 }
+
 .my_header_color_text {
   color: $color-text-primary;
 
@@ -90,7 +95,7 @@ export default {
 .my_btn_dropdown {
   background-color: $color-secondary;
   border-color: $color-black;
-  width:  100%;
+  width: 100%;
   height: 1.875rem;
   border-radius: 5%;
   border: none;
@@ -105,7 +110,7 @@ export default {
 .my_dropdown_style {
   background-color: $color-secondary;
   border-color: $color-black;
-  width:  12.5rem;
+  width: 12.5rem;
   border-radius: 5%;
   transition: all 0.3s ease-in-out;
 
