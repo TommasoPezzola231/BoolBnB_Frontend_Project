2f0f0e4364d8b46b467bc0450caee974d9509c84
TommasoPezzola <pezzola123@gmail.com>
2023-09-06 19:04:09 +0200

    fix layout

--- src/components/main/search-apartments/ResultApartments.vue
+++ src/components/main/search-apartments/ResultApartments.vue
@@ -24,61 +24,87 @@ export default {
 </script>
 
 <template>
-    <div class="row mb-4">
-        <div class="col-7">
 
 
-            <h2 class="ms-4">Risultati ricerca</h2>
-            <p>Appartamenti</p>
 
+    <div class="row mb-4 w-100 mx-auto">
+        <h2 class="text-center">Risultati ricerca</h2>
+        <button type="button" class="btn btnCustom" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+            Ricerca Avanzata
+        </button>
+
+        <!-- Show loading spinner when loading is true -->
+        <div v-if="loading" class="spinner-border text-primary" role="status">
+            <span class="visually-hidden">Loading...</span>
+        </div>
+
+        <div v-else>
             <div v-if="store.apartments.length === 0">
-                <p>Nessun appartamento trovato.</p>
+                <p class="position-absolute top-50 start-50 translate-middle fs-1">Nessun appartamento trovato</p>
             </div>
 
-
-            <div class="row mb-4">
-                <h2 class="ms-4">Risultati ricerca</h2>
-                <p>Appartamenti</p>
-
-                <!-- Show loading spinner when loading is true -->
-                <div v-if="loading" class="spinner-border text-primary" role="status">
-                    <span class="visually-hidden">Loading...</span>
-                </div>
-
-                <div v-else>
-                    <div v-if="store.apartments.length === 0">
-                        <p>Nessun appartamento trovato.</p>
-                    </div>
-
-                    <div v-else>
-                        <div v-for="apartment in store.apartments" :key="apartment.id" class="mb-3 card col-10 mx-auto">
-                            <!-- Visualizza le informazioni dell'appartamento -->
-                            <h3>{{ apartment.title }}</h3>
-                            <small>{{ apartment.city }}</small>
-                            <p>{{ apartment.description }}</p>
-                            <p>{{ apartment.address }}</p>
-                            <p>Numero bagni: {{ apartment.num_bathrooms }}</p>
-                            <p>Numero stanze:{{ apartment.num_rooms }}</p>
-                            <p>{{ apartment.square_meters }} metri quadri</p>
-                            <ul>
-                                <template v-for="service in apartment.services" :key="service">
-                                    <li class="badge text-bg-info me-2">{{ service.name_service }}</li>
-                                </template>
-                            </ul>
-                            <p>Prezzo: {{ apartment.price }}€</p>
-                            <!-- ... altri dettagli dell'appartamento ... -->
-                            <router-link :to="{ name: 'apartment-show', params: { id: apartment.id } }"
-                                class="stretched-link"></router-link>
-
-                            <router-link :to="{ name: 'apartment-show', params: { id: apartment.id } }"
-                                class="stretched-link"></router-link>
+            <div v-else class="row p-2">
+                <div v-for="apartment in store.apartments" :key="apartment.id" class="col-lg-4 col-md-6 col-sm-12">
+                    
+                    
+                <router-link class="" :to="{ name: 'apartment-show', params: { id: apartment.id } }">
+                    <div class="card-apartment shadow p-1 rounded-4 my-3">
+                        <!-- Visualizza le informazioni dell'appartamento -->
+                    
+                        <img class="w-100 rounded" :src="apartment.principal_image ?  'http://localhost:8000/storage/' + apartment.principal_image : 'https://www.signfix.com.au/wp-content/uploads/2017/09/placeholder-600x400.png'">
+                        <h3>{{ apartment.title }}</h3>
+                        <p>{{ apartment.address }} {{ apartment.city }}</p>
+                        <div class="row">
+                            <p class="col-4 text-center"><font-awesome-icon :icon="['fas', 'toilet']" /> {{ apartment.num_bathrooms }}</p>
+                            <p class="col-4 text-center"><font-awesome-icon :icon="['fas', 'house']" /> {{ apartment.num_rooms }}</p>
+                            <p class="col-4 text-center"><font-awesome-icon :icon="['fas', 'ruler-combined']" /> {{ apartment.square_meters }} metri quadri</p>
 
                         </div>
+                        <ul>
+                            <template v-for="service in apartment.services" :key="service">
+                                <li class="badge text-bg-info prova me-2">
+                                    {{ service.name_service }}
+                                    <font-awesome-icon :icon="service.icon_service" />
+                                </li>
+                            </template>
+                        </ul>
+                        <p class="text-end me-2">
+                            <span>
+                                Prezzo: {{ apartment.price }}€
+                            </span>
+                        </p>
                     </div>
+                </router-link>
+
                 </div>
             </div>
         </div>
     </div>
+        
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.prova{
+    background-color: #4158D0;
+    background-image: linear-gradient(43deg, #4158D0 0%, #C850C0 46%, #FFCC70 100%);
+}
+
+.btnCustom{    
+    background-color: #4158D0;
+    background-image: linear-gradient(43deg, #4158D0 0%, #C850C0 46%, #FFCC70 100%);
+    color: white;
+    &:hover{
+        box-shadow: 5px 5px 15px rgba(0, 0, 0, 0.2);
+        background-color: #3a4fba;
+    }
+}
+
+a{
+    text-decoration: none;
+    color: black;
+}
+
+.card-apartment{
+    background-color: white;
+}
+</style>
--- src/components/main/search-apartments/SearchPlus.vue
+++ src/components/main/search-apartments/SearchPlus.vue
@@ -17,6 +17,7 @@ export default {
             price: "",
             selectedServices: [],
             loading: false,
+            menuVisible: false,
         };
     },
 
@@ -46,6 +47,7 @@ export default {
     methods: {
         async searchAdvancedApartment() {
             try {
+                console.log(this.store.services)
                 this.loading = true; // Show loading indicator
                 const response = await axios.get(`http://localhost:8000/api/ricercaAvanzata`, {
                     params: {
@@ -60,6 +62,7 @@ export default {
                 });
                 console.log(this.selectedServices)
                 console.log(response)
+                
                 // Chiamata alla mutazione per salvare i risultati nello store
                 this.store.apartments = response.data;
                 console.log(this.store.apartments);
@@ -70,74 +73,128 @@ export default {
                 this.loading = false;
             }
         },
+        toggleMenu() {
+            this.menuVisible = !this.menuVisible; // Inverti lo stato del menu
+        },
     }
 }
 </script>
 
 <template>
-    <div class="container-form">
+    
+    
+    <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="staticBackdropLabel">Ricerca Avanzata</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
 
 
-        <div class="row mb-4">
-            <h2 class="ms-4">Ricerca Avanzata</h2>
-            <div class="container">
+        <div class="container-form ps-3 col-12 ricercaAvanzata position-relative">
+
+            
+            <div class="menuContainer">
+                <div class="row mb-4">
+                    <div class="container">
 
 
-                <form @submit.prevent="searchAdvancedApartment()" method="get">
-                    <div class="row flex-column">
+                        <form @submit.prevent="searchAdvancedApartment()" method="get">
+                            <div class="row flex-column">
 
-                        <div class="col-3 p-2">
-                            <label for="radius" class="form-label">Imposta raggio di ricerca</label>
-                            <input type="number" class="form-control" v-model="radius" id="radius" name="radius" required>
-                        </div>
+                                <div class="col-11 p-2">
+                                    <label for="radius" class="form-label">Imposta raggio di ricerca</label>
+                                    <input type="range" class="form-range" v-model="radius" id="radius" name="radius" required min="0" max="50">
+                                    <div class="text-center mx-auto">
+                                        <span class="border rounded p-1">{{radius}}  Km</span>
+                                    </div>
+                                </div>
 
-                        <div class="col-3 p-2">
-                            <label for="city" class="form-label">Città</label>
-                            <input type="text" class="form-control" v-model="city" id="city" name="city" required>
-                        </div>
+                                <div class="col-11 p-2">
+                                    <label for="city" class="form-label">Città <font-awesome-icon :icon="['fas', 'location-dot']" /></label>
+                                    <input type="text" class="form-control" v-model="city" id="city" name="city" required>
+                                </div>
+                                
+                                <div class="d-flex">
+                                    <div class="col-6 p-2">
+                                        <label for="num_rooms" class="form-label">Numero Stanze <font-awesome-icon :icon="['fas', 'house']" /></label>
+                                        <input type="number" class="form-control" v-model="num_rooms" id="num_rooms" name="num_rooms"
+                                        min="1" max="15">
+                                    </div>
+                                    
+                                    <div class="col-6 p-2">
+                                        <label for="num_bathrooms" class="form-label">Numero Bagni <font-awesome-icon :icon="['fas', 'toilet']" /></label>
+                                        <input type="number" class="form-control" v-model="num_bathrooms" id="num_bathrooms"
+                                        name="num_bathrooms" min="1" max="7">
+                                    </div>
+                                </div>
 
-                        <div class="col-3 p-2">
-                            <label for="num_rooms" class="form-label">Numero Stanze</label>
-                            <input type="number" class="form-control" v-model="num_rooms" id="num_rooms" name="num_rooms"
-                                min="1" max="15">
-                        </div>
+                            </div>
 
-                        <div class="col-3 p-2">
-                            <label for="num_bathrooms" class="form-label">Numero Bagni</label>
-                            <input type="number" class="form-control" v-model="num_bathrooms" id="num_bathrooms"
-                                name="num_bathrooms" min="1" max="7">
-                        </div>
 
+                            <div class="row flex-column">
+
+                                <div class="d-flex">
+                                    <div class="col-6 p-2">
+                                        <label for="square_meters" class="form-label">Metri quadri <font-awesome-icon :icon="['fas', 'ruler-combined']" /></label>
+                                        <input type="number" class="form-control" v-model="square_meters" id="square_meters"
+                                        name="square_meters" min="1" max="400">
+                                    </div>
+                                    
+                                    <div class="col-6 p-2">
+                                        <label for="price" class="form-label">Prezzo <font-awesome-icon :icon="['fas', 'euro-sign']" /></label>
+                                        <input type="number" class="form-control" v-model="price" id="price" name="price">
+                                    </div>
+                                </div>
+                                    
+                                <div class="col-11 p-2">
+
+                                    <label v-for="(service, index) in this.store.services" :key="index + 1" class="dropdown-item my-3 service">
+                                        <input class="d-none" type="checkbox" :value="index + 1" v-model="selectedServices" name="serviceID">
+                                        <span class="p-2 border rounded" :class="{ 'selected-service': selectedServices.includes(index + 1) }">
+                                            <font-awesome-icon :icon="service.icon_service" /> {{ service.name_service }}
+                                        </span>
+                                    </label>
+                                </div>
+
+                                <button type="submit" class="btn btn-secondary col-2 mx-auto" data-bs-dismiss="modal" aria-label="Close">Cerca</button>
+
+                            </div>
+                            
+                        </form>
                     </div>
-
-
-                    <div class="row flex-column">
-
-                        <div class="col-3 p-2">
-                            <label for="square_meters" class="form-label">Metri quadri</label>
-                            <input type="number" class="form-control" v-model="square_meters" id="square_meters"
-                                name="square_meters" min="1" max="400">
-                        </div>
-
-                        <div class="col-3 p-2">
-                            <label for="price" class="form-label">Prezzo</label>
-                            <input type="number" class="form-control" v-model="price" id="price" name="price">
-                        </div>
-
-                        <div class="col-3 p-2">
-
-                            <label v-for="(service, index) in this.store.services" :key="index + 1" class="dropdown-item">
-                                <input type="checkbox" :value="index + 1" v-model="selectedServices" name="serviceID">
-                                {{ service.name_service }}
-                            </label>
-                        </div>
-
-                    </div>
-                    <button type="submit" class="btn btn-primary">Submit</button>
-                </form>
+                </div>
             </div>
-        </div>
+            
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+    
+
     </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.service{
+  cursor: pointer;
+}
+.selected-service {
+    background-color: #4158D0;
+    background-image: linear-gradient(43deg, #4158D0 0%, #C850C0 46%, #FFCC70 100%);
+}
+
+.closebtn {
+  cursor: pointer;
+  font-size: 36px;
+    &:hover{
+        color: red;
+    }
+}
+
+</style>
